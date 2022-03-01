1419a3a459fc7a4ec19f259b762f5ae096b0c0ac
Gracie Gumm <gracie.e.gumm@vanderbilt.edu>
2022-03-01 14:50:07 -0600

    Update quickStart.py

--- quickStart.py
+++ quickStart.py
@@ -1,14 +1,33 @@
-import ../iTRAC as it
+import iTRAC as it
+
+
+#below is stuff I have added
+import numpy as np
+import matplotlib.pyplot as pt
+import csv
+import pandas as pd
+import cantools
+import matplotlib.animation as animation
+from matplotlib import style
+from haversine import haversine, Unit
+import itertools
+import math
+import strym as s
+import networkx as nx
+import os
+
+import xlwt #for exporting as excel book
+#end of stuff I have added
 
 
 #replace with your filepath
-data = pd.read_csv('/Volumes/SSD_MNICE/vanderdata/2T3MWRFVXLW056972/libpanda/2021_08_06/2021-08-06-11-40-19_2T3MWRFVXLW056972_CAN_Messages.csv')
+data = pd.read_csv('/media/ggrumm/Extreme SSD/2021-08-04-14-53-43_2T3MWRFVXLW056972_CAN_Messages.csv')
 
 #replace with your filepath
-db2 = s.initializeDBC_Cantools('../strym/strym/dbc/toyota_rav4_hybrid.dbc')
+db2 = s.initializeDBC_Cantools('/home/ggrumm/strym/strym/dbc/toyota_rav4_hybrid.dbc')
 
 
 radarData = it.allRadar(data)
 G = it.myGraph(radarData,dt=0.5)
 p = it.SSP(G.G)
-dfs = it.getPathDfs(p)
+dfs = it.getPathDfs(p, G) #added G as parameter
